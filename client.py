8bfeacf300d35d03ea7f1eb9b2fa62e1352351ee
boysimon10 <simondiouf2710@gmail.com>
2025-03-03 21:58:38 +0000

    fix: update client to use utf-8 encoding

--- client.py
+++ client.py
@@ -9,9 +9,9 @@ client.connect(('127.0.0.1', 5000))
 def receive():
     while True:
         try:
-            message = client.recv(1024).decode('ascii')
+            message = client.recv(1024).decode('utf-8')
             if message == 'USERNAME':
-                client.send(username.encode('ascii'))
+                client.send(username.encode('utf-8'))
             else:
                 print(message)
         except:
@@ -22,7 +22,7 @@ def receive():
 def write():
     while True:
         message = f'{username}: {input("")}'
-        client.send(message.encode('ascii'))
+        client.send(message.encode('utf-8'))
 
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
